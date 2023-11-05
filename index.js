cb2c38de587d02e6ced2e6d5a9f6cc40de255546
javier-wq <javiermonge247@gmail.com>
2023-11-05 13:42:49 -0600

    Routes

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile('./static/index.html', {
-        root: __dirname
-    })
+    res.send('Hello World')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('The current wheather is nice')
+})
+
+app.use((req, res) => {
+    res.status(404).send('Not Found')
 })
 
 app.listen(3000)
