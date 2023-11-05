3dffc23c0d7a81aca5f79deca02738a98a9c18b0
javier-wq <javiermonge247@gmail.com>
2023-11-05 13:05:59 -0600

    Testing Express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hola Mundo');
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile('./static/index.html', {
+        root: __dirname
+    })
+})
+
+app.listen(3000)
+console.log(`Server on port ${3000}`)
